1ee4d460213cb4ac576d9065e4e80532daad4c10
Swati Bajaj <swatibajaj54@gmail.com>
2022-06-12 23:08:15 +0530

    frequent pattern-with-code-refactoring

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-
-
-def readFile(filename):
+def readFile(filename):  # returns an error message
     try:
         input = open(filename)
         content = input.readlines()
@@ -11,7 +8,9 @@ def readFile(filename):
         return IOError("file not found")
 
 
-def getMinMaxSequences(content):
+def seqs_dict(
+    content,
+):  # returns a dictionary from the fasta file and number of records in the file
     seqs = {}
     for line in content:
         line = line.rstrip()
@@ -21,20 +20,17 @@ def getMinMaxSequences(content):
             seqs[name] = ""
         else:
             seqs[name] = seqs[name] + line
+    print("records number in file:", len(seqs.keys()))
+    return seqs
 
-    # print("Number of sequences in input file is", len(seqs.keys()))
+
+def getMinMaxSequences(
+    seqs,
+):  # returns lengths of the sequences in the file, longest sequence, shortest sequence and their identifiers
     sequence_length = []
-    # max_length = 0
-    # min_length = math.inf
     for name in seqs:
         length = len(seqs[name])
         sequence_length.append(length)
-    #     if length > max_length:
-    #         max_length = length
-    #     if length < min_length:
-    #         min_length = length
-    # print(name, max_length)
-    # print(name, min_length)
     max_length = max(sequence_length)
     min_length = min(sequence_length)
     max_length_identifier = []
@@ -44,19 +40,12 @@ def getMinMaxSequences(content):
             max_length_identifier.append(name)
         if len(seqs[name]) == min_length:
             min_length_identifier.append(name)
-    # print("sequence lengths is:", sequence_length)
-    # print(
-    #     "max length:",
-    #     max_length,
-    #     max_length_identifier,
-    #     "min length:",
-    #     min_length,
-    #     min_length_identifier,
-    # )
-    return seqs
+    return max_length_identifier, max_length, min_length_identifier, min_length
 
 
-def orf_count(start_position, seqs):
+def orf_count(
+    start_position, seqs
+):  # returns orf with maximum length, its index and identifier.
     orfs_dict = {}
     for name in seqs:
         seq = seqs[name]
@@ -64,31 +53,44 @@ def orf_count(start_position, seqs):
         while i <= len(seq) - 3:
             codon = seq[i : i + 3]
             if codon == "ATG":
-                text = seq[i : len(seq) - 3]
-                indexOfTAA = text.find("TAA")
-                indexOfTAG = text.find("TAG")
-                indexOfTGA = text.find("TGA")
-                endIndex = -1
-                if indexOfTAA != -1:
-                    endIndex = indexOfTAA
-                elif indexOfTAG != -1:
-                    endIndex = indexOfTAG
-                elif indexOfTGA != -1:
-                    endIndex = indexOfTGA
-
-                if endIndex != -1:
-                    orf = text[0 : endIndex + 3]
-                    orfs_dict[f"{name}_{i}"] = orf
+                j = i
+                while j <= len(seq) - 3:
+                    stop_codon = seq[j : j + 3]
+                    if stop_codon in ["TAA", "TAG", "TGA"]:
+                        orf = seq[i : j + 3]
+                        orfs_dict[f"{name}_{i}"] = orf
+                        break
+                    j = j + 3
             i = i + 3
-    print(orfs_dict)
-    for name_index in orfs_dict:
-        length = len(orfs_dict[name_index])
-        max_length = 0
+    max_length = 0
+    for key, value in orfs_dict.items():
+        length = len(value)
         if length > max_length:
             max_length = length
     for key, val in orfs_dict.items():
         if len(val) == max_length:
-            print(key, val, max_length)
+            return key, val, max_length
+
+
+def repeat_pattern(n, seqs):  # returns pattern of length n that has max frequency
+    pattern_dict = {}
+    for key in seqs:
+        seq = seqs[key]
+        i = 0
+        while i <= len(seq) - n:
+            pattern = seq[i : i + n]
+            pattern_dict[pattern] = pattern_dict.get(pattern, 0) + 1
+            i = i + 1
+    max_frequency = 0
+    key = ""
+    for pattern_key in pattern_dict:
+        frequency_pattern = pattern_dict[pattern_key]
+        if frequency_pattern > max_frequency:
+            max_frequency = frequency_pattern
+            key = pattern_key
+    for pattern in pattern_dict:
+        if pattern_dict[pattern] == max_frequency:
+            return pattern, max_frequency
 
 
 def main():
@@ -102,8 +104,16 @@ def main():
         print("Terminating program: no content in the file")
         return
 
-    dict = getMinMaxSequences(content)
-    orf_count(0, dict)
+    dict = seqs_dict(content)
+    min_max_seq = getMinMaxSequences(dict)
+    print(
+        "max_length_identifier, max_length, min_length_identifier, min_length:",
+        min_max_seq,
+    )
+    orf = orf_count(0, dict)
+    print("seq identifier with max orf length_index, orf, length:", orf)
+    frequent_pattern = repeat_pattern(7, dict)
+    print("frequent occuring pattern of length n and its frequency:", frequent_pattern)
 
 
 main()
